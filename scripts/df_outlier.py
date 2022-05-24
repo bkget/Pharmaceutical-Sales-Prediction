8d92262ef8c0e1b1ba6a747471eddcf5c0b97d4b
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-24 05:45:06 +0300

    replace outliers with IQR rule

--- scripts/df_outlier.py
+++ scripts/df_outlier.py
@@ -35,3 +35,15 @@ class DfOutlier:
       lower, upper = Q1 - cut_off, Q3 + cut_off
       self.df = self.df.drop(self.df[self.df[col] > upper].index)
       self.df = self.df.drop(self.df[self.df[col] < lower].index)
+
+  def replace_outliers_with_iqr(self, columns):
+    for col in columns:
+      Q1, Q3 = self.df[col].quantile(0.25), self.df[col].quantile(0.75)
+      IQR = Q3 - Q1
+      cut_off = IQR * 1.5
+      lower, upper = Q1 - cut_off, Q3 + cut_off
+
+      self.df[col] = np.where(self.df[col] > upper, upper, self.df[col])
+      self.df[col] = np.where(self.df[col] < lower, lower, self.df[col])
+
+  
